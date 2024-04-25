f7978bc00bf255d30b27f8c4c364d27755fe0e1c
bijay kumar yadav <ybijayyadav468@gmail.com>
2024-04-25 19:06:14 +0530

    using google authentication

--- config/environment.js
+++ config/environment.js
@@ -13,8 +13,6 @@ const development = {
       pass: "lfph ikmp brwh yiby",
     },
   },
-  google_clientID:
-    "499369908385-d16fqo965eur07pm8k3cdqleac3unms1.apps.googleusercontent.com",
   google_callbackURL: "http://localhost:8000/users/auth/google/callback",
   jwt_secret_or_key: "fb_login_clone",
 };
